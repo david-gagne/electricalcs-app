d9c1db11b73ed43cfd4da8408aab0a891b6ab280
david-gagne <dgagne813@gmail.com>
2018-09-02 08:26:50 -0400

    Fixed script loading issue for mobile menu functionality, removed window.onload

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-window.onload = function () {
-    // landing page cta button 
-    let ctaButton = document.querySelector('.custom-button-width');
+// landing page cta button 
+let ctaButton = document.querySelector('.custom-button-width');
 
-    // remove button when clicked
-    ctaButton.addEventListener('click', function () {
-        ctaButton.style.display = 'none';
-    });
-}
+// remove button when clicked
+ctaButton.addEventListener('click', function () {
+    ctaButton.style.display = 'none';
+});
