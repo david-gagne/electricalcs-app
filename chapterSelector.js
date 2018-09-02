b7cae1eceefc030ca5663946da84f894b9f470e8
david-gagne <dgagne813@gmail.com>
2018-09-02 11:30:48 -0400

    Updated chapter page and chapter quiz container (styles and scripts)

--- chapterSelector.js
+++ chapterSelector.js
@@ -4,11 +4,11 @@ window.onload = function () {
     chapterContainer.addEventListener('click', function (event) {
         switch (event.target) {
             // Toggle display CHAPTER ONE (1) Quizlet embed
-            case chapterContainer.children[0].children[0]:
+            case chapterContainer.children[0]: // indexes are counted by twos (2s).
                 document.getElementById('chapterOneQuiz').classList.toggle('hide-quiz');
                 break;
 
-            case chapterContainer.children[1].children[0]:
+            case chapterContainer.children[2]:
                 document.getElementById('chapterTwoQuiz').classList.toggle('hide-quiz');
                 break;
 
