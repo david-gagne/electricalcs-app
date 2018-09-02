c4a6a6ebbcbd671a3987aa18c65bf9f36ace568c
david-gagne <dgagne813@gmail.com>
2018-09-02 18:06:21 -0400

    Updated feature request link on burger menu and html overflow function when burger menu is active

--- mobileMenu.js
+++ mobileMenu.js
@@ -1,14 +1,16 @@
 // declare and define all mobile menu variables
 let burgerMenu = document.getElementById('mobile-menu-button');
 let mobileMenu = document.getElementById('mobile-menu');
+let html = document.querySelector('html');
 
 // attach click event listener to burger menu
 burgerMenu.addEventListener('click', function () {
     burgerMenu.classList.toggle('is-active');
     if (mobileMenu.style.display !== 'block') {
         mobileMenu.style.display = 'block';
-        
+        html.style.overflow = 'hidden';
     } else {
         mobileMenu.style.display = 'none';
+        html.style.overflow = 'unset';
     }
 });
