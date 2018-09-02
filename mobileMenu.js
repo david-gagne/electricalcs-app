3e1a78f03f005b85014bcb068c773a1f9829b99b
david-gagne <dgagne813@gmail.com>
2018-09-02 08:27:51 -0400

    Edited script linked to mobile menu issue

--- mobileMenu.js
+++ mobileMenu.js
@@ -1,22 +1,13 @@
-window.onload = function () {
-    // Burger menu activation
-    // define mobile menu variables
-    let mobileMenuButton = document.getElementById('mobile-menu-button');
-    let mobileMenu = document.getElementById('mobile-menu');
+// declare and define all mobile menu variables
+let burgerMenu = document.getElementById('mobile-menu-button');
+let mobileMenu = document.getElementById('mobile-menu');
 
-    // attach listener to mobile menu button (burger)
-    mobileMenuButton.addEventListener('click', function () {
-        // check if the menu modal is actie and burger icon is cross (x) 
-        if (!mobileMenuButton.classList.contains('is-active')) {
-            // if menu is not active and icon is not cross, then make them so...
-            mobileMenuButton.classList.add('is-active');
-            mobileMenu.style.display = 'block';
-            // and make it so that the user cannot scroll off the modal...
-            document.querySelector('html').style.overflow = 'hidden';
-        } else if (mobileMenuButton.classList.contains('is-active')) {
-            mobileMenuButton.classList.remove('is-active');
-            mobileMenu.style.display = 'none';
-            document.querySelector('html').style.overflow = 'visible';
-        }
-    });
-}
+// attach click event listener to burger menu
+burgerMenu.addEventListener('click', function () {
+    burgerMenu.classList.toggle('is-active');
+    if (mobileMenu.style.display !== 'block') {
+        mobileMenu.style.display = 'block';
+    } else {
+        mobileMenu.style.display = 'none';
+    }
+});
