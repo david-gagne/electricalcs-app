49a1e373057799751fa4b700fedd8a9ed4f3b604
david-gagne <dgagne813@gmail.com>
2018-10-13 21:10:26 -0400

    Added scripts for chapter 5 quiz questions

--- chapterSelector.js
+++ chapterSelector.js
@@ -20,6 +20,10 @@ window.onload = function () {
                 document.getElementById('chapterFourQuiz').classList.toggle('hide-quiz');
                 break;
 
+            case chapterContainer.children[8]:
+                document.getElementById('chapterFiveQuiz').classList.toggle('hide-quiz');
+                break;
+
             default:
                 console.log('nada clicked');
                 break;
